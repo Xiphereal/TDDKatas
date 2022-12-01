1a1f187121ac2e74966a3cfe92224ad4b9975e97
Xiphereal <lozanojalex@gmail.com>
2022-12-01 21:46:48 +0100

    Remove duplicated class

--- AdventOfCode2022/Day1/Model/LinesToElfs.cs
+++ AdventOfCode2022/Day1/Model/LinesToElfs.cs
@@ -2,11 +2,51 @@
 {
     public class LinesToElfs
     {
-        private List<string> lines;
+        private readonly IEnumerable<string> lines;
 
-        public LinesToElfs(List<string> lines)
+        public LinesToElfs(IEnumerable<string> lines)
         {
             this.lines = lines;
+        }
+
+        public IEnumerable<Elf> Elfs
+        {
+            get
+            {
+                List<Elf> elfs = new();
+
+                for (int i = 0; i < lines.Count(); i++)
+                {
+                    string line = lines.ElementAt(i);
+
+                    if (IsTheCaloriesOfAFood(line))
+                    {
+                        List<int> elfCarriedFood = new()
+                        {
+                            int.Parse(line)
+                        };
+
+                        while (HasNext(i) && IsTheCaloriesOfAFood(lines.ElementAt(++i)))
+                        {
+                            elfCarriedFood.Add(int.Parse(lines.ElementAt(i)));
+                        }
+
+                        elfs.Add(new Elf(elfCarriedFood.ToArray()));
+                    }
+                }
+
+                return elfs;
+
+                bool HasNext(int i)
+                {
+                    return i < lines.Count() - 1;
+                }
+
+                static bool IsTheCaloriesOfAFood(string line)
+                {
+                    return int.TryParse(line, out _);
+                }
+            }
         }
     }
 }
