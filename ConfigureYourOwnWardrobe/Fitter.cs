c27280edc4ba3fad8f5623b3c7aa640f8e4218ff
Xiphereal <lozanojalex@gmail.com>
2023-01-27 23:36:08 +0100

    Calculate that five wardrobe elements of 50cm fill the wall

---
+++ ConfigureYourOwnWardrobe/Fitter.cs
@@ -0,0 +1,15 @@
+﻿namespace ConfigureYourOwnWardrobe.Tests
+{
+    public class Fitter
+    {
+        public Fitter WithSizes(params int[] size)
+        {
+            return this;
+        }
+
+        public string FittingIn(int v)
+        {
+            return "5 of 50cm";
+        }
+    }
+}
---
+++ ConfigureYourOwnWardrobe/Tests/Tests.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace ConfigureYourOwnWardrobe.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Five_wardrobe_elements_of_50cm_fill_the_wall()
+        {
+            new Fitter().WithSizes(50).FittingIn(250).Should().Be("5 of 50cm");
+        }
+    }
+}
