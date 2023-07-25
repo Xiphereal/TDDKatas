9375da777bb317ae2c753df7ff9696be52e98971
Xiphereal <lozanojalex@gmail.com>
2023-07-25 21:29:31 +0200

    test: several duplicated set of numbers also result in a noncompliant matrix

--- SudokuKata/Tests/UnitTests.cs
+++ SudokuKata/Tests/UnitTests.cs
@@ -37,6 +37,18 @@ namespace SudokuKata.Tests
         }
 
         [Fact]
+        public void Matrix_with_several_duplicated_numbers_in_both_columns_and_rows_is_noncompliant()
+        {
+            var matrix = new Matrix();
+            matrix.Rows.Add(new List<int> { 2, 1, 3, 4 });
+            matrix.Rows.Add(new List<int> { 3, 4, 1, 2 });
+            matrix.Rows.Add(new List<int> { 2, 3, 4, 1 });
+            matrix.Rows.Add(new List<int> { 3, 1, 2, 4 });
+
+            matrix.CheckForViolations().Should().Be("The input doesn't comply with Sudoku's rules.");
+        }
+
+        [Fact]
         public void Columns_can_be_obtained_from_rows()
         {
             var matrix = new Matrix();
