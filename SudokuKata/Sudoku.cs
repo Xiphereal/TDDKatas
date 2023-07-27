b5a3e4c8382641615ec4112c1a4ab7644a7f0274
Xiphereal <lozanojalex@gmail.com>
2023-07-27 18:14:01 +0200

    feat: correct proposed solution is recognized as such

---
+++ SudokuKata/Sudoku.cs
@@ -0,0 +1,17 @@
+﻿namespace SudokuKata.Tests.UnitTests
+{
+    public class Sudoku
+    {
+        private Matrix initialGrid;
+
+        public Sudoku(Matrix initialGrid)
+        {
+            this.initialGrid = initialGrid;
+        }
+
+        public string IsSolution(Matrix matrix)
+        {
+            return "The proposed solution is correct";
+        }
+    }
+}
---
+++ SudokuKata/Tests/UnitTests/SudokuTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Xunit;
+using static SudokuKata.Tests.Builders.MatrixBuilder;
+
+namespace SudokuKata.Tests.UnitTests
+{
+    public class SudokuTests
+    {
+        [Fact]
+        public void TestName()
+        {
+            Matrix initialGrid =
+                Matrix()
+                    .WithRow(new EmptyCell(), new Cell(1))
+                    .WithRow(new Cell(1), new Cell(2))
+                    .Build();
+
+            var sut = new Sudoku(initialGrid);
+
+            sut.IsSolution(
+                Matrix()
+                    .WithRow(new Cell(2), new Cell(1))
+                    .WithRow(new Cell(1), new Cell(2))
+                    .Build())
+                .Should().Be("The proposed solution is correct");
+        }
+    }
+}
