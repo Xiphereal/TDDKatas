5e6dba018ac9b6ab57d787a2a3e3409621e6f8f9
Joser Gala Naranjo <jgalanar@gmail.com>
2023-08-01 18:39:32 +0200

    feat: land at position

--- MarsRoverKata/Tests/PlateausTests.cs
+++ MarsRoverKata/Tests/PlateausTests.cs
@@ -1,56 +1,58 @@
 using FluentAssertions;
+using System;
 using Xunit;
 
 namespace MarsRoverKata.Tests
 {
-    public class PlateausTests
-    {
-        [Fact]
-        public void PlateauIs10x10()
-        {
-            var sut = new Plateau();
+	public class PlateausTests
+	{
+		[Fact]
+		public void PlateauIs10x10()
+		{
+			var sut = new Plateau();
 
-            sut.x.Should().Be(10);
-            sut.y.Should().Be(10);
-        }
+			sut.x.Should().Be(10);
+			sut.y.Should().Be(10);
+		}
 
-        [Fact]
-        public void LandRover_OnPlateauPosition()
-        {
-            var plateau = new Plateau();
-            var rover = new Rover(plateau);
+		[Fact]
+		public void LandRover_OnPlateauPosition()
+		{
+			var plateau = new Plateau();
+			var rover = new Rover(plateau);
 
-            rover.LandAt(0, 0);
+			rover.LandAt(0, 0);
 
-            rover.position.x.Should().Be(0);
-            rover.position.y.Should().Be(0);
-        }
-    }
+			rover.Position.Item1.Should().Be(0);
+			rover.Position.Item2.Should().Be(0);
+		}
+	}
 
-    public class Rover
-    {
-        public Tuple<int,int> x;
-        
-        public Rover(Plateau plateau)
-        {
-            throw new System.NotImplementedException();
-        }
+	public class Rover
+	{
+		public Tuple<int, int> Position;
+		private readonly Plateau plateau;
 
-        public void LandAt(int i, int i1)
-        {
-            
-        }
-    }
+		public Rover(Plateau plateau)
+		{
+			this.plateau = plateau;
+		}
 
-    public class Plateau
-    {
-        public readonly int x;
-        public readonly int y;
+		public void LandAt(int i, int i1)
+		{
+			Position = new Tuple<int, int>(i, i1);
+		}
+	}
 
-        public Plateau()
-        {
-            x = 10;
-            y = 10;
-        }
-    }
+	public class Plateau
+	{
+		public readonly int x;
+		public readonly int y;
+
+		public Plateau()
+		{
+			x = 10;
+			y = 10;
+		}
+	}
 }
