a39e509d2d456f87c3a26a4ae520d7177ae2122d
Victor Moreno <>
2023-08-01 18:52:49 +0200

    feat: move forward

--- MarsRoverKata/Tests/PlateausTests.cs
+++ MarsRoverKata/Tests/PlateausTests.cs
@@ -26,10 +26,35 @@ namespace MarsRoverKata.Tests
 			rover.Position.Item1.Should().Be(0);
 			rover.Position.Item2.Should().Be(0);
 		}
+
+		[Fact]
+		public void MoveRover_Forward()
+		{
+			var plateau = new Plateau();
+			var rover = new Rover(plateau);
+
+			rover.LandAt(0, 0);
+			rover.MoveForward();
+
+			rover.Position.Item1.Should().Be(0);
+			rover.Position.Item2.Should().Be(1);
+		}
+
+		[Fact]
+		public void CheckRoverOrientation()
+		{
+			var plateau = new Plateau();
+			var rover = new Rover(plateau);
+
+			rover.LandAt(0, 0);
+
+			rover.Orientation.Should().Be(Orientation.North);
+		}
 	}
 
 	public class Rover
 	{
+		public Orientation Orientation { get; set; }
 		public Tuple<int, int> Position;
 		private readonly Plateau plateau;
 
@@ -41,6 +66,11 @@ namespace MarsRoverKata.Tests
 		public void LandAt(int i, int i1)
 		{
 			Position = new Tuple<int, int>(i, i1);
+		}
+
+		public void MoveForward()
+		{
+			Position = new Tuple<int, int>(Position.Item1, Position.Item2 + 1);
 		}
 	}
 
@@ -54,5 +84,10 @@ namespace MarsRoverKata.Tests
 			x = 10;
 			y = 10;
 		}
+	}
+
+	public enum Orientation
+	{
+		North
 	}
 }
