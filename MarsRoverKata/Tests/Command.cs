4d583ed367b2023ecb852d7c17f7441f08f57412
Iván Moreno <imoreno.owo@gmail.com>
2023-08-01 20:27:07 +0200

    refactor: introduce command notion

---
+++ MarsRoverKata/Tests/Command.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MarsRoverKata.Tests
+{
+    public readonly struct Command
+    {
+        readonly char whatHasToBeDone;
+    
+        Command(char whatHasToBeDone)
+        {
+            if (whatHasToBeDone != 'M' || whatHasToBeDone != 'R' || whatHasToBeDone != 'L')
+                throw new ArgumentException("Invalid command");
+        
+            this.whatHasToBeDone = whatHasToBeDone;
+        }
+
+        public static implicit operator Command(char command) => new Command(command);
+        public static implicit operator char(Command command) => command.whatHasToBeDone;
+    }
+}
