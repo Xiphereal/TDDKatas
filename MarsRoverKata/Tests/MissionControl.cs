cf0bb6f5e7d518f7832e740ed92b22edf2b4d498
RGV <ricardo.guzman.velasco@outlook.com>
2023-08-01 20:11:14 +0200

    feat: order to rover from mission control

---
+++ MarsRoverKata/Tests/MissionControl.cs
@@ -0,0 +1,16 @@
+﻿namespace MarsRoverKata.Tests;
+
+public class MissionControl
+{
+    readonly Rover doc;
+
+    public MissionControl(Rover doc)
+    {
+        this.doc = doc;
+    }
+
+    public void Order(string input)
+    {
+        doc.MoveForward();
+    }
+}
---
+++ MarsRoverKata/Tests/ParserTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace MarsRoverKata.Tests;
+
+public class ParserTests
+{
+	[Fact]
+	public void alksjdf()
+	{
+		var otroDocmás = new Plateau(); 
+		var doc = new Rover(otroDocmás);
+
+		var sut = new MissionControl(doc);
+		sut.Order("M");
+
+		doc.Position.Should().Be((0, 1));
+	}
+}
