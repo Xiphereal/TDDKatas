f2263fef693f4e858e2699e374d05fedc38fb74d
Joser Gala Naranjo <jgalanar@gmail.com>
2023-08-01 19:42:07 +0200

    feat: move to forward direction

--- MarsRoverKata/Tests/PlateausTests.cs
+++ MarsRoverKata/Tests/PlateausTests.cs
@@ -3,102 +3,117 @@ using Xunit;
 
 namespace MarsRoverKata.Tests
 {
-    public class PlateausTests
-    {
-        [Fact]
-        public void PlateauIs10x10()
-        {
-            var sut = new Plateau();
+	public class PlateausTests
+	{
+		[Fact]
+		public void PlateauIs10x10()
+		{
+			var sut = new Plateau();
 
-            sut.x.Should().Be(10);
-            sut.y.Should().Be(10);
-        }
+			sut.x.Should().Be(10);
+			sut.y.Should().Be(10);
+		}
 
-        [Fact]
-        public void LandRover_OnPlateauPosition()
-        {
-            var plateau = new Plateau();
-            var rover = new Rover(plateau);
+		[Fact]
+		public void LandRover_OnPlateauPosition()
+		{
+			var plateau = new Plateau();
+			var rover = new Rover(plateau);
 
-            rover.LandAt(0, 0);
+			rover.LandAt(0, 0);
 
-            rover.Position.Item1.Should().Be(0);
-            rover.Position.Item2.Should().Be(0);
-        }
+			rover.Position.x.Should().Be(0);
+			rover.Position.y.Should().Be(0);
+		}
 
-        [Fact]
-        public void MoveRover_Forward()
-        {
-            var plateau = new Plateau();
-            var rover = new Rover(plateau);
+		[Fact]
+		public void MoveRover_Forward()
+		{
+			var plateau = new Plateau();
+			var rover = new Rover(plateau);
 
-            rover.LandAt(0, 0);
-            rover.MoveForward();
+			rover.LandAt(0, 0);
+			rover.MoveForward();
 
-            rover.Position.Item1.Should().Be(0);
-            rover.Position.Item2.Should().Be(1);
-        }
+			rover.Position.x.Should().Be(0);
+			rover.Position.y.Should().Be(1);
+		}
 
-        [Fact]
-        public void CheckRoverOrientation()
-        {
-            var plateau = new Plateau();
-            var rover = new Rover(plateau);
+		[Fact]
+		public void CheckRoverOrientation()
+		{
+			var plateau = new Plateau();
+			var rover = new Rover(plateau);
 
-            rover.LandAt(0, 0);
+			rover.LandAt(0, 0);
 
-            rover.Orientation.Should().Be(CardinalPoint.North);
-        }
+			rover.Orientation.Should().Be(CardinalPoint.North);
+		}
 
-        [Fact]
-        public void RotateRight()
-        {
-            var plateau = new Plateau();
-            var rover = new Rover(plateau);
-            rover.LandAt(0, 0);
+		[Fact]
+		public void RotateRight()
+		{
+			var plateau = new Plateau();
+			var rover = new Rover(plateau);
+			rover.LandAt(0, 0);
 
-            rover.RotateRight();
+			rover.RotateRight();
 
-            rover.Orientation.Should().Be(CardinalPoint.East);
-        }
+			rover.Orientation.Should().Be(CardinalPoint.East);
+		}
 
-        [Fact]
-        public void RotateLeft()
-        {
-            var plateau = new Plateau();
-            var rover = new Rover(plateau);
-            rover.LandAt(0, 0);
+		[Fact]
+		public void RotateLeft()
+		{
+			var plateau = new Plateau();
+			var rover = new Rover(plateau);
+			rover.LandAt(0, 0);
 
-            rover.RotateLeft();
+			rover.RotateLeft();
 
-            rover.Orientation.Should().Be(CardinalPoint.West);
-        }
+			rover.Orientation.Should().Be(CardinalPoint.West);
+		}
 
-        [Fact]
-        public void TurnAround()
-        {
-            var plateau = new Plateau();
-            var rover = new Rover(plateau);
-            rover.LandAt(0, 0);
+		[Fact]
+		public void TurnAround()
+		{
+			var plateau = new Plateau();
+			var rover = new Rover(plateau);
+			rover.LandAt(0, 0);
 
-            rover.RotateLeft();
-            rover.RotateLeft();
+			rover.RotateLeft();
+			rover.RotateLeft();
 
-            rover.Orientation.Should().Be(CardinalPoint.South);
-        }
+			rover.Orientation.Should().Be(CardinalPoint.South);
+		}
 
-        [Fact]
-        public void RotateRightThenMoveForwards()
-        {
-            var plateau = new Plateau();
-            var rover = new Rover(plateau);
-            rover.LandAt(0, 0);
+		[Fact]
+		public void RotateRightThenMoveForwards()
+		{
+			var plateau = new Plateau();
+			var rover = new Rover(plateau);
+			rover.LandAt(0, 0);
 
-            rover.RotateRight();
-            rover.MoveForward();
+			rover.RotateRight();
+			rover.MoveForward();
 
-            rover.Position.Item1.Should().Be(1);
-            rover.Position.Item2.Should().Be(0);
-        }
-    }
+			rover.Position.x.Should().Be(1);
+			rover.Position.y.Should().Be(0);
+		}
+
+		[Fact]
+		public void SJDFSDVBDASK()
+		{
+			var plateau = new Plateau();
+			var rover = new Rover(plateau);
+			rover.LandAt(0, 0);
+
+			rover.RotateRight();
+			rover.RotateRight();
+			rover.MoveForward();
+
+			rover.Position.y.Should().Be(-1);
+			rover.Position.x.Should().Be(0);
+		}
+	}
 }
--- MarsRoverKata/Tests/Rover.cs
+++ MarsRoverKata/Tests/Rover.cs
@@ -1,40 +1,58 @@
-using System;
-
 namespace MarsRoverKata.Tests;
 
 public class Rover
 {
-    public CardinalPoint Orientation { get; set; }
-    public (int x, int y) Position => plateau.WhereRoverIs;
+	public CardinalPoint Orientation { get; set; }
+	public (int x, int y) Position => plateau.WhereRoverIs;
 
-    private readonly Plateau plateau;
+	private readonly Plateau plateau;
 
-    public Rover(Plateau plateau)
-    {
-        this.plateau = plateau;
-    }
+	public Rover(Plateau plateau)
+	{
+		this.plateau = plateau;
+	}
 
-    public void LandAt(int x, int y)
-    {
-        plateau.Receive(this, x, y);
-    }
+	public void LandAt(int x, int y)
+	{
+		plateau.Receive(this, x, y);
+	}
 
-    public void MoveForward()
-    {
-        var forward = Orientation == CardinalPoint.East
-            ? (Position.Item1 + 1, Position.Item2)
-            : (Position.Item1, Position.Item2 + 1);
+	public void MoveForward()
+	{
+		var forward = Direction();
 
-        plateau.Receive(this, forward.Item1, forward.Item2);
-    }
+		plateau.Receive(this, forward.Item1, forward.Item2);
+	}
 
-    public void RotateRight()
-    {
-        Orientation = Orientation.RotateRight();
-    }
+	private (int, int) Direction()
+	{
+		switch (Orientation)
+		{
+			case CardinalPoint.North:
+				return (0, 1);
+			case CardinalPoint.East:
 
-    public void RotateLeft()
-    {
-        Orientation = Orientation.RotateLeft();
-    }
+				break;
+			case CardinalPoint.South:
+				break;
+			case CardinalPoint.West:
+				break;
+			default:
+				break;
+		}
+
+		return Orientation == CardinalPoint.East
+			? (Position.x + 1, Position.y)
+			: (Position.x, Position.y + 1);
+	}
+
+	public void RotateRight()
+	{
+		Orientation = Orientation.RotateRight();
+	}
+
+	public void RotateLeft()
+	{
+		Orientation = Orientation.RotateLeft();
+	}
 }
