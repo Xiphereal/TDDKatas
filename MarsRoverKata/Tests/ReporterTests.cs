c05378f0f272efd58a8fa610953175ab12f3b758
Victor Moreno <>
2023-08-02 20:03:06 +0200

    test: report after movement

--- MarsRoverKata/Tests/ReporterTests.cs
+++ MarsRoverKata/Tests/ReporterTests.cs
@@ -14,5 +14,16 @@ namespace MarsRoverKata.Tests
 
             rover.Report().Should().Be("5:5:N");
         }
+
+        [Fact]
+        public void ReportsPositionAfterMoving()
+        {
+            var plateau = new Plateau();
+            var rover = new Rover(plateau);
+            rover.LandAt(5, 5);
+            rover.MoveForward();
+
+            rover.Report().Should().Be("5:6:N");
+        }
     }
 }
