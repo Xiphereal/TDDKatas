1bdafe1caad9641aa6fce8100cbf2be93ed184e9
Xiphereal <lozanojalex@gmail.com>
2024-05-23 23:17:34 +0200

    chore: initial for RacingCarTirePressure

---
+++ RacingCarTirePressure/Tests/AlarmTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace TirePressure
+{
+    public class AlarmTests
+    {
+        [Fact]
+        public void AlarmIsOffByDefault()
+        {
+            Alarm alarm = new Alarm();
+            Assert.False(alarm.AlarmOn);
+        }
+    }
+}
