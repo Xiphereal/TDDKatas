a85e5b24f5b07b9282712095fed4cbb4b3fd9362
Xiphereal <lozanojalex@gmail.com>
2024-06-08 14:44:04 +0200

    refactor: move UpdateQuality method to new Inventory class

---
+++ GildedRose/Inventory.cs
@@ -0,0 +1,102 @@
+﻿
+namespace GildedRose.Console
+{
+    public class Inventory
+    {
+        private IList<Item> Items = [];
+
+        public static Inventory Empty => new();
+
+        public Inventory With(Item item)
+        {
+            Items.Add(item);
+
+            return this;
+        }
+
+        public Inventory With(IList<Item> items)
+        {
+            Items = items.ToList();
+
+            return this;
+        }
+
+        public void UpdateQuality()
+        {
+            for (var i = 0; i < Items.Count; i++)
+            {
+                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (Items[i].Quality > 0)
+                    {
+                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            Items[i].Quality = Items[i].Quality - 1;
+                        }
+                    }
+                }
+                else
+                {
+                    if (Items[i].Quality < 50)
+                    {
+                        Items[i].Quality = Items[i].Quality + 1;
+
+                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (Items[i].SellIn < 11)
+                            {
+                                if (Items[i].Quality < 50)
+                                {
+                                    Items[i].Quality = Items[i].Quality + 1;
+                                }
+                            }
+
+                            if (Items[i].SellIn < 6)
+                            {
+                                if (Items[i].Quality < 50)
+                                {
+                                    Items[i].Quality = Items[i].Quality + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    Items[i].SellIn = Items[i].SellIn - 1;
+                }
+
+                if (Items[i].SellIn < 0)
+                {
+                    if (Items[i].Name != "Aged Brie")
+                    {
+                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (Items[i].Quality > 0)
+                            {
+                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                {
+                                    Items[i].Quality = Items[i].Quality - 1;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                        }
+                    }
+                    else
+                    {
+                        if (Items[i].Quality < 50)
+                        {
+                            Items[i].Quality = Items[i].Quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+}
--- GildedRose/Tests/GildedRoseInventoryTests.cs
+++ GildedRose/Tests/GildedRoseInventoryTests.cs
@@ -1,7 +1,35 @@
+using FluentAssertions;
+using GildedRose.Console;
+using Xunit;
+
 namespace GildedRose.Tests
 {
     public class GildedRoseInventoryTests
     {
+        // Max quality is 50
+        // Min quality is 0
+        // Each pass day lowers quality & sell in values
+        // aged brie increases quality the older it gets
+        // backstage passes increase quality by 2 when there are 10 days or less
+        // backstage passes increase quality by 3 when there are 5 days or less
+        // backstage passes has quality 0 after the SellIn expires
+        // Sulfuras has no SellIn nor quality decrement
+        // When SellIn date has passed, quality degrades twice as fast
 
+        // feat: conjured items degrade quality twice as fast as normal ones
+
+        [Fact]
+        public void SellInReducesBy1_AfterEachDayPasses()
+        {
+            Item item = new Item()
+            {
+                SellIn = 1,
+            };
+            var sut = Inventory.Empty.With(item);
+
+            sut.UpdateQuality();
+
+            item.SellIn.Should().Be(0);
+        }
     }
 }
