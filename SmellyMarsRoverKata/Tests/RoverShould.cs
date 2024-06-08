f15724311446b0e99ddc2b5758e2b3bd9ba2d0c9
Xiphereal <lozanojalex@gmail.com>
2024-06-08 02:25:55 +0200

    test: displacing towards facing direction

--- SmellyMarsRoverKata/Tests/RoverShould.cs
+++ SmellyMarsRoverKata/Tests/RoverShould.cs
@@ -13,5 +13,54 @@ namespace MarsRoverKata.Tests
             sut.Y.Should().Be(0);
             sut.Direction.Should().Be("N");
         }
+
+        [Fact]
+        public void DisplaceTowardsFacingDirection()
+        {
+            MovesNorth();
+            MovesSouth();
+            MovesEast();
+            MovesWest();
+        }
+
+        private static void MovesNorth()
+        {
+            var sut = new Rover(0, 0, "N");
+
+            sut.Receive("f");
+
+            sut.X.Should().Be(0);
+            sut.Y.Should().Be(1);
+        }
+
+        private static void MovesSouth()
+        {
+            var sut = new Rover(0, 0, "S");
+
+            sut.Receive("f");
+
+            sut.X.Should().Be(0);
+            sut.Y.Should().Be(-1);
+        }
+
+        private static void MovesEast()
+        {
+            var sut = new Rover(0, 0, "E");
+
+            sut.Receive("f");
+
+            sut.X.Should().Be(1);
+            sut.Y.Should().Be(0);
+        }
+
+        private static void MovesWest()
+        {
+            var sut = new Rover(0, 0, "W");
+
+            sut.Receive("f");
+
+            sut.X.Should().Be(-1);
+            sut.Y.Should().Be(0);
+        }
     }
 }
