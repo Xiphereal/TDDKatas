c893b1a31e3123f1abf15f7475e4484f325c090c
Xiphereal <lozanojalex@gmail.com>
2024-06-09 21:32:17 +0200

    refactor: use primary constructor also removing fields

--- GildedRose/Inventory.cs
+++ GildedRose/Inventory.cs
@@ -1,95 +1,85 @@
 ﻿
 namespace GildedRose.Console
 {
-    public class Inventory
+    public class Inventory(IList<Item> items)
     {
         private const string AgedBrie = "Aged Brie";
         private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
         private const string BackstagePasses = "Backstage passes to a TAFKAL80ETC concert";
         private const int MaxQuality = 50;
 
-        private IList<Item> Items;
-
-        public Inventory(IList<Item> items)
-        {
-            this.Items = items;
-        }
-
-        public static Inventory With(IList<Item> items)
-        {
-            return new Inventory(items);
-        }
+        public static Inventory With(IList<Item> items) => new Inventory(items);
 
         public void UpdateQuality()
         {
-            for (var i = 0; i < Items.Count; i++)
+            for (var i = 0; i < items.Count; i++)
             {
-                if (Items[i].Name != AgedBrie && Items[i].Name != BackstagePasses)
+                if (items[i].Name != AgedBrie && items[i].Name != BackstagePasses)
                 {
-                    if (Items[i].Quality > 0)
+                    if (items[i].Quality > 0)
                     {
-                        if (Items[i].Name != Sulfuras)
+                        if (items[i].Name != Sulfuras)
                         {
-                            Items[i].Quality = Items[i].Quality - 1;
+                            items[i].Quality = items[i].Quality - 1;
                         }
                     }
                 }
                 else
                 {
-                    if (Items[i].Quality < MaxQuality)
+                    if (items[i].Quality < MaxQuality)
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
+                        items[i].Quality = items[i].Quality + 1;
 
-                        if (Items[i].Name == BackstagePasses)
+                        if (items[i].Name == BackstagePasses)
                         {
-                            if (Items[i].SellIn < 11)
+                            if (items[i].SellIn < 11)
                             {
-                                if (Items[i].Quality < MaxQuality)
+                                if (items[i].Quality < MaxQuality)
                                 {
-                                    Items[i].Quality = Items[i].Quality + 1;
+                                    items[i].Quality = items[i].Quality + 1;
                                 }
                             }
 
-                            if (Items[i].SellIn < 6)
+                            if (items[i].SellIn < 6)
                             {
-                                if (Items[i].Quality < MaxQuality)
+                                if (items[i].Quality < MaxQuality)
                                 {
-                                    Items[i].Quality = Items[i].Quality + 1;
+                                    items[i].Quality = items[i].Quality + 1;
                                 }
                             }
                         }
                     }
                 }
 
-                if (Items[i].Name != Sulfuras)
+                if (items[i].Name != Sulfuras)
                 {
-                    Items[i].SellIn = Items[i].SellIn - 1;
+                    items[i].SellIn = items[i].SellIn - 1;
                 }
 
-                if (Items[i].SellIn < 0)
+                if (items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != AgedBrie)
+                    if (items[i].Name != AgedBrie)
                     {
-                        if (Items[i].Name != BackstagePasses)
+                        if (items[i].Name != BackstagePasses)
                         {
-                            if (Items[i].Quality > 0)
+                            if (items[i].Quality > 0)
                             {
-                                if (Items[i].Name != Sulfuras)
+                                if (items[i].Name != Sulfuras)
                                 {
-                                    Items[i].Quality = Items[i].Quality - 1;
+                                    items[i].Quality = items[i].Quality - 1;
                                 }
                             }
                         }
                         else
                         {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                            items[i].Quality = items[i].Quality - items[i].Quality;
                         }
                     }
                     else
                     {
-                        if (Items[i].Quality < MaxQuality)
+                        if (items[i].Quality < MaxQuality)
                         {
-                            Items[i].Quality = Items[i].Quality + 1;
+                            items[i].Quality = items[i].Quality + 1;
                         }
                     }
                 }
