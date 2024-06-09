e6c4673699637b1db2fb90bbe31bf2d9e5e3e1d3
Xiphereal <lozanojalex@gmail.com>
2024-06-09 19:02:08 +0200

    docs: add test list

--- GildedRose/Tests/GildedRoseTests.cs
+++ GildedRose/Tests/GildedRoseTests.cs
@@ -2,6 +2,18 @@ namespace GildedRose.Tests
 {
     public class GildedRoseInventoryTests
     {
-
+        // Quality degrades afte reach day
+        // SellIn is reduced after each day
+        // Quality cannot go below 0
+        // Quality cannot go above 50
+        // Legendary Items do not degrade nor have sellIn
+        // Aged Brie increases quality the older it gets
+        // Once the sell by date has passed, Quality degrades twice as fast
+        // What about Aged Brie when sell by date has passed????????
+        // Backstage passes increases quality the older it gets
+        // Backstage passes increases quality by 2 when 10 days remains
+        // Backstage passes increases quality by 3 when 5 days remains
+        // Backstage passes has quality drop to 0 when they are expired
+        // Conjured items degrade in Quality twice as fast as normal items
     }
 }
