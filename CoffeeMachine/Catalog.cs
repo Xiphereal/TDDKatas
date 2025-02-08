a5dad863cd226c8ea3b530b00dd61295abcd4bae
Xiphereal <lozanojalex@gmail.com>
2025-02-08 21:44:23 +0100

    refactor: make Catalog a collection of strings

--- CoffeeMachine/Catalog.cs
+++ CoffeeMachine/Catalog.cs
@@ -1,9 +1,25 @@
-﻿namespace CoffeeMachine;
+﻿using System.Collections;
 
-public class Catalog(params string[] coffes)
+namespace CoffeeMachine;
+
+public class Catalog(params string[] coffes) : IReadOnlyList<string>
 {
     public IReadOnlyList<string> ASDfasdf()
     {
         return coffes;
     }
+
+    public IEnumerator<string> GetEnumerator()
+    {
+        return ((IEnumerable<string>)coffes).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public int Count => coffes.Length;
+
+    public string this[int index] => coffes[index];
 }
